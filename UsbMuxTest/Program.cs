8b7dedf0281a514182fb4e93c370ef5b42cff032
Erik Faye-Lund <kusmabite@gmail.com>
2017-12-21 13:48:46 +0100

    reindent Program.cs
    UsbMux.cs is indented with spaces, so let's have Program.cs indented
    the same way.

--- UsbMuxTest/Program.cs
+++ UsbMuxTest/Program.cs
@@ -4,46 +4,46 @@ using Fuse.UsbMux;
 
 namespace UsbMuxText
 {
-	class MainClass
-	{
+    class MainClass
+    {
 
-		public static void Main(string[] args)
-		{
-			try
-			{
-				AppKit.NSApplication.Init();
+        public static void Main(string[] args)
+        {
+            try
+            {
+                AppKit.NSApplication.Init();
 
-				var devices = UsbMux.ListDevices();
-				foreach (var d in devices)
-				{
-					try
-					{
-						Console.WriteLine(string.Format("device: {0}", d.DeviceID));
-						var stream = UsbMux.Connect(d, 1337);
-						var streamReader = new StreamReader(stream);
-						var hello = streamReader.ReadLine();
-						Console.WriteLine("phone says: {0}", hello);
-						var streamWriter = new StreamWriter(stream);
-						streamWriter.WriteLine("HELLO TO YOU TOO, PHONE!\n");
-						streamWriter.Flush();
-					}
-					catch (FailedToConnectToDevice e)
-					{
-						Console.WriteLine("failed to connect to device: {0}", e.ToString());
-					}
-				}
+                var devices = UsbMux.ListDevices();
+                foreach (var d in devices)
+                {
+                    try
+                    {
+                        Console.WriteLine(string.Format("device: {0}", d.DeviceID));
+                        var stream = UsbMux.Connect(d, 1337);
+                        var streamReader = new StreamReader(stream);
+                        var hello = streamReader.ReadLine();
+                        Console.WriteLine("phone says: {0}", hello);
+                        var streamWriter = new StreamWriter(stream);
+                        streamWriter.WriteLine("HELLO TO YOU TOO, PHONE!\n");
+                        streamWriter.Flush();
+                    }
+                    catch (FailedToConnectToDevice e)
+                    {
+                        Console.WriteLine("failed to connect to device: {0}", e.ToString());
+                    }
+                }
 
-				var deviceListener = new DeviceListener();
-				deviceListener.DeviceAttached += (object sender, DeviceListener.DeviceEventArgs a) => Console.WriteLine("attached: " + a.Device.ToString());
-				deviceListener.DeviceDetached += (object sender, DeviceListener.DeviceEventArgs a) => Console.WriteLine("detached: " + a.Device.ToString());
+                var deviceListener = new DeviceListener();
+                deviceListener.DeviceAttached += (object sender, DeviceListener.DeviceEventArgs a) => Console.WriteLine("attached: " + a.Device.ToString());
+                deviceListener.DeviceDetached += (object sender, DeviceListener.DeviceEventArgs a) => Console.WriteLine("detached: " + a.Device.ToString());
 
-				while (true)
-					deviceListener.Poll();
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine("fatal exception: {0}", e.ToString());
-			}
-		}
-	}
+                while (true)
+                    deviceListener.Poll();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("fatal exception: {0}", e.ToString());
+            }
+        }
+    }
 }
